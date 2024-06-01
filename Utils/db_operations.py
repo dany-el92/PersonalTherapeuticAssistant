a11722e88be264710927b5f0945efe1bfe13d375
luigina <l.costante1@studenti.unisa.it>
2024-06-01 13:54:48 +0200

    aggiunta indexing e covered query

--- Utils/db_operations.py
+++ Utils/db_operations.py
@@ -4,13 +4,13 @@ from pymongo.errors import ConnectionFailure, OperationFailure
 
 class Database:
     """
-       Classe per gestire la connessione e le operazioni su un database MongoDB per un chatbot.
+    Classe per gestire la connessione e le operazioni su un database MongoDB per un chatbot.
 
-       Attributi:
-           uri (str): l'URI di connessione a MongoDB.
-           db_name (str): il nome del database da utilizzare.
-           client (MongoClient): oggetto client di MongoDB.
-           db (Database): oggetto del database.
+    Attributi:
+        uri (str): L'URI di connessione a MongoDB.
+        db_name (str): Il nome del database da utilizzare.
+        client (MongoClient): L'oggetto client di MongoDB.
+        db (Database): L'oggetto del database.
     """
 
     def __init__(self, uri='mongodb://localhost:27017/', db_name='chatbot_db'):
@@ -19,14 +19,13 @@ class Database:
         self.client = None
         self.db = None
         self.connect_to_db()
+        self.create_indexes()
 
     def connect_to_db(self):
-
         """
-            Connette al database MongoDB e imposta l'oggetto del database.
-            Gestisce le eccezioni in caso di fallimento della connessione.
+        Connette al database MongoDB e imposta l'oggetto del database.
+        Gestisce le eccezioni in caso di fallimento della connessione.
         """
-
         try:
             self.client = MongoClient(self.uri)
             self.db = self.client[self.db_name]
@@ -35,10 +34,26 @@ class Database:
         except OperationFailure as e:
             print(f"Operation failed: {e}")
 
+    def create_indexes(self):
+        """
+        Crea indici sulle collezioni per migliorare le prestazioni delle query.
+        """
+        try:
+            self.db['patterns'].create_index([('tag', 1), ('patterns', 1)])
+            self.db['responses'].create_index([('tag', 1), ('responses', 1)])
+        except Exception as e:
+            print(f"Error creating indexes: {e}")
+
     def get_all_documents(self, collection_name):
+        """
+        Recupera tutti i documenti da una collezione specificata.
 
-        # Funzione che recupera tutti i documenti da una collezione specificata.
+        Args:
+            collection_name (str): Il nome della collezione da cui recuperare i documenti.
 
+        Returns:
+            list: Una lista di documenti dalla collezione.
+        """
         try:
             collection = self.db[collection_name]
             return list(collection.find({}))
@@ -47,9 +62,16 @@ class Database:
             return []
 
     def get_documents_by_tag(self, collection_name, tag):
+        """
+        Recupera i documenti da una collezione specificata in base a un tag.
 
-        # Funzione che recupera i documenti da una collezione specificata in base a un tag.
+        Args:
+            collection_name (str): Il nome della collezione da cui recuperare i documenti.
+            tag (str): Il tag per filtrare i documenti.
 
+        Returns:
+            list: Una lista di documenti che corrispondono al tag dalla collezione.
+        """
         try:
             collection = self.db[collection_name]
             return list(collection.find({"tag": tag}))
@@ -57,10 +79,36 @@ class Database:
             print(f"Error retrieving documents by tag: {e}")
             return []
 
+    def get_documents_by_tag_covered(self, collection_name, tag):
+        """
+        Recupera i documenti da una collezione specificata in base a un tag,
+        utilizzando una query covered.
+
+        Args:
+            collection_name (str): Il nome della collezione da cui recuperare i documenti.
+            tag (str): Il tag per filtrare i documenti.
+
+        Returns:
+            list: Una lista di documenti che corrispondono al tag dalla collezione.
+        """
+        try:
+            collection = self.db[collection_name]
+            if collection_name == "patterns":
+                return list(collection.find({"tag": tag}, {"tag": 1, "patterns": 1, "_id": 0}))
+            else:
+                return list(collection.find({"tag": tag}, {"tag": 1, "responses": 1, "_id": 0}))
+        except Exception as e:
+            print(f"Error retrieving covered documents by tag: {e}")
+            return []
+
     def insert_document(self, collection_name, document):
+        """
+        Inserisce un documento in una collezione specificata.
 
-        # Funzione che inserisce un documento in una collezione specificata.
-
+        Args:
+            collection_name (str): Il nome della collezione in cui inserire il documento.
+            document (dict): Il documento da inserire.
+        """
         try:
             collection = self.db[collection_name]
             collection.insert_one(document)
@@ -68,56 +116,86 @@ class Database:
             print(f"Error inserting document: {e}")
 
     def get_all_patterns(self):
+        """
+        Recupera tutti i documenti dalla collezione 'patterns'.
 
-        # Funzione che recupera tutti i documenti dalla collezione 'patterns'.
-
+        Returns:
+            list: Una lista di documenti dalla collezione 'patterns'.
+        """
         return self.get_all_documents('patterns')
 
     def get_all_responses(self):
+        """
+        Recupera tutti i documenti dalla collezione 'responses'.
 
-        # Funzione che recupera tutti i documenti dalla collezione 'responses'.
-
+        Returns:
+            list: Una lista di documenti dalla collezione 'responses'.
+        """
         return self.get_all_documents('responses')
 
     def get_patterns_by_tag(self, tag):
+        """
+        Recupera i documenti dalla collezione 'patterns' in base a un tag.
 
-        # Funzione che recupera i documenti dalla collezione 'patterns' in base a un tag.
+        Args:
+            tag (str): Il tag per filtrare i documenti.
 
+        Returns:
+            list: Una lista di documenti che corrispondono al tag dalla collezione 'patterns'.
+        """
         return self.get_documents_by_tag('patterns', tag)
 
     def get_responses_by_tag(self, tag):
+        """
+        Recupera i documenti dalla collezione 'responses' in base a un tag.
 
-        # Funzione che recupera i documenti dalla collezione 'responses' in base a un tag.
+        Args:
+            tag (str): Il tag per filtrare i documenti.
 
+        Returns:
+            list: Una lista di documenti che corrispondono al tag dalla collezione 'responses'.
+        """
         return self.get_documents_by_tag('responses', tag)
 
     def insert_pattern(self, tag, patterns):
+        """
+        Inserisce un nuovo documento nella collezione 'patterns'.
 
-        # Funzione che inserisce un nuovo documento nella collezione 'patterns'.
-
+        Args:
+            tag (str): Il tag del documento.
+            patterns (list): La lista dei pattern associati al tag.
+        """
         document = {"tag": tag, "patterns": patterns}
         self.insert_document('patterns', document)
 
     def insert_response(self, tag, responses):
+        """
+        Inserisce un nuovo documento nella collezione 'responses'.
 
-        # Funzione che inserisce un nuovo documento nella collezione 'responses'.
-
+        Args:
+            tag (str): Il tag del documento.
+            responses (list): La lista delle risposte associate al tag.
+        """
         document = {"tag": tag, "responses": responses}
         self.insert_document('responses', document)
 
 
 # Esempio di utilizzo delle funzioni
 if __name__ == "__main__":
+    # Creazione dell'istanza del database, che crea automaticamente gli indici
     db = Database()
 
     # Esempio di select
-    print(db.get_all_patterns())
-    print(db.get_all_responses())
+    # print(db.get_all_patterns())
+    # print(db.get_all_responses())
     print(db.get_patterns_by_tag('test_tag'))
     print(db.get_responses_by_tag('test_tag'))
+
+    # Esempio di covered query
+    print(db.get_documents_by_tag_covered('patterns', 'greeting'))
 
     # Esempio di insert
     # db.insert_pattern('test_tag2', ['Hello', 'Hi'])
     # db.insert_response('test_tag2', ['Hello there!', 'Hi!'])
 
-    # DA CANCELLARE (test_tag e test_tag2 sia in pattern che in response)
+    # DA CANCELLARE patter e response con test_tag e test_tag2
