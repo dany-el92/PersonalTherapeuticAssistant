15d6a282862fe4544bfd6b48bb0d7c69cb296da2
luigina <l.costante1@studenti.unisa.it>
2024-06-13 18:01:50 +0200

    bug fix

--- Dataset/dataset_analysis.py
+++ Dataset/dataset_analysis.py
@@ -29,34 +29,6 @@ print(expanded_df.head())
 
 # Statistiche aggiuntive e grafici
 
-# Intenti unici nel dataset
-print("Intenti unici nel dataset:")
-print(expanded_df['tag'].unique())
-print(f"Numero totale di intenti unici nel dataset: {len(expanded_df['tag'].unique())}")
-
-
-# Grafico 1: Distribuzione degli Intenti
-intent_counts = expanded_df['tag'].value_counts()
-fig = go.Figure(data=[go.Bar(x=intent_counts.index, y=intent_counts.values)])
-fig.update_layout(title='Distribuzione degli Intenti', xaxis_title='Intenti', yaxis_title='Conteggio')
-fig.show()
-
-# Lunghezza media dei pattern e delle risposte per ogni intento
-expanded_df_with_lengths = expanded_df
-expanded_df_with_lengths['pattern_length'] = expanded_df['pattern'].apply(len)
-expanded_df_with_lengths['response_length'] = expanded_df['response'].apply(len)
-
-avg_pattern_length = expanded_df_with_lengths.groupby('tag')['pattern_length'].mean()
-avg_response_length = expanded_df_with_lengths.groupby('tag')['response_length'].mean()
-
-# Grafico 2: Analisi della Lunghezza di Pattern e Risposte per Intento
-fig = go.Figure()
-fig.add_trace(go.Bar(x=avg_pattern_length.index, y=avg_pattern_length.values, name='Lunghezza Media dei Pattern'))
-fig.add_trace(go.Bar(x=avg_response_length.index, y=avg_response_length.values, name='Lunghezza Media delle Risposte'))
-fig.update_layout(title='Analisi della Lunghezza di Pattern e Risposte', xaxis_title='Intenti',
-                  yaxis_title='Lunghezza Media')
-fig.show()
-
 print("---------------------------------------------------------------------------------------------------------")
 
 print("INFO")
@@ -66,3 +38,33 @@ print("-------------------------------------------------------------------------
 
 statistiche = expanded_df.describe(include='all')
 print(f"STATISTICHE:\n{statistiche}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+# Intenti unici nel dataset
+print("Intenti unici nel dataset:")
+print(expanded_df['tag'].unique())
+print(f"Numero totale di intenti unici nel dataset: {len(expanded_df['tag'].unique())}")
+
+print("---------------------------------------------------------------------------------------------------------")
+
+# Grafico 1: Distribuzione degli Intenti
+intent_counts = expanded_df['tag'].value_counts()
+fig = go.Figure(data=[go.Bar(x=intent_counts.index, y=intent_counts.values)])
+fig.update_layout(title='Distribuzione degli Intenti', xaxis_title='Intenti', yaxis_title='Conteggio')
+fig.show()
+
+# Lunghezza media dei pattern e delle risposte per ogni intento
+expanded_df['pattern_length'] = expanded_df['pattern'].apply(len)
+expanded_df['response_length'] = expanded_df['response'].apply(len)
+
+avg_pattern_length = expanded_df.groupby('tag')['pattern_length'].mean()
+avg_response_length = expanded_df.groupby('tag')['response_length'].mean()
+
+# Grafico 2: Analisi della Lunghezza di Pattern e Risposte per Intento
+fig = go.Figure()
+fig.add_trace(go.Bar(x=avg_pattern_length.index, y=avg_pattern_length.values, name='Lunghezza Media dei Pattern'))
+fig.add_trace(go.Bar(x=avg_response_length.index, y=avg_response_length.values, name='Lunghezza Media delle Risposte'))
+fig.update_layout(title='Analisi della Lunghezza di Pattern e Risposte', xaxis_title='Intenti',
+                  yaxis_title='Lunghezza Media')
+fig.show()
